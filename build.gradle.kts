05d19561bc7f64e3a9116dc5ac1bc49a9601efd5
Aurelijus Graužinis <aurelijus.grauzinis@gmail.com>
2019-04-18 23:30:52 +0300

    first commit to github

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+@file:Suppress("SpellCheckingInspection")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot") version "2.1.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    kotlin("jvm") version "1.3.30"
+    kotlin("plugin.spring") version "1.3.30"
+}
+
+group = "com.microservices"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation("org.amshove.kluent:kluent:1.49")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.named<BootJar>("bootJar") {
+    launchScript()
+}
